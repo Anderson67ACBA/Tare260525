cc1a822e1eec98ed707b224f9656b2e66beb2333
Anderson Chaleb <Anderson Chaleb@DESKTOP-QV270QB>
2025-05-26 01:06:36 -0500

    modificacion

--- ListaAlumnos.java
+++ ListaAlumnos.java
@@ -7,6 +7,9 @@ public class ListaAlumnos {
         lista.add(new Alumno("001", "Ana Pérez", "A12345"));
         lista.add(new Alumno("002", "Luis Gómez", "B23456"));
         lista.add(new Alumno("003", "María López", "C34567"));
+        lista.add(new Alumno("004", "Carlos Ruiz", "D45678"));
+        lista.add(new Alumno("005", "Sofía Martínez", "E56789"));
+        lista.add(new Alumno("006", "Jorge Díaz", "F67890"));
 
         for (Alumno a : lista) {
             System.out.println(a);
