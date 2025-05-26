f0d387789338b4dc17efebe5923fdc3cbcd628b8
Anderson Chaleb <Anderson Chaleb@DESKTOP-QV270QB>
2025-05-26 02:52:18 -0500

    Ejer10Finalizado

---
+++ EjercicioPag32/Ejer10/Barco.java
@@ -0,0 +1,13 @@
+package EjercicioPag32.Ejer10;
+
+public class Barco extends Carro {
+       public Barco(String marca, String modelo, String color) {
+        super(marca, modelo, color);
+    }
+
+    @Override
+    public void mover() {
+        System.out.println("El barco navega .");
+
+    }
+}
