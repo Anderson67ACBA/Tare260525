24c2a4276014809d026b89532684d9cd1c3ceed3
Anderson Chaleb <Anderson Chaleb@DESKTOP-QV270QB>
2025-05-26 02:38:13 -0500

    JuegosCompleto

---
+++ EjercicioPag32/Main2.java
@@ -0,0 +1,12 @@
+package EjercicioPag32;
+
+public class Main2 {
+    public static void main(String[] args) {
+        Nivel2 obj = new Nivel2();
+        obj.imprimePreguntas();
+
+        System.out.println("MARCADOR");
+        System.out.println("Nivel 2");
+        System.out.println("Tu puntaje es:" + obj.puntaje);
+    }
+}
